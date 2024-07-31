b7266c3d98d29f9d8ff979e9d1be06cca84561d9
MilanaMila <milana.cool2903@gmail.com>
2024-07-31 11:33:01 +0300

    rabota10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,106 +1,42 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import iconBell from '/img/bi_bell.png';
-import successIcon from '/img/bi_check2-circle.png';
-import iconOctagon from '/img/bi_x-octagon.png';
-
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', handlerCreatePromise);
+form.addEventListener('submit', handlerSubmit);
 
-document.addEventListener('DOMContentLoaded', event => {
-  iziToast.info({
-    position: 'topRight',
-    title: 'Hello',
-    message: 'WELCOME!',
-    titleColor: 'white',
-    titleSize: '16px',
-    messageColor: 'white',
-    backgroundColor: '#09f',
-    iconUrl: iconBell,
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const data = {};
+  new FormData(event.target).forEach((value, key) => {
+    data[key] = value;
   });
-});
 
-function handlerCreatePromise(evt) {
-  evt.preventDefault();
-  const { delay, state } = evt.target.elements;
-  const delayNumber = Number(delay.value);
-  const stateArr = [...state].find(item => item.checked);
+  const delay = data.delay;
+  const state = data.state;
 
-  createPromise(delayNumber, stateArr)
-    .then(delayNumber => {
-      iziToast.success({
-        position: 'topRight',
-        title: 'OK',
-        message: `Fulfilled promise in ${delayNumber}ms`,
-        titleColor: 'white',
-        titleSize: '16px',
-        messageColor: 'white',
-        backgroundColor: '#59a10d',
-        iconUrl: successIcon,
-      });
-    })
-    .catch(delayNumber => {
-      iziToast.error({
-        position: 'topRight',
-        title: 'Error',
-        message: `Rejected promise in ${delayNumber}ms`,
-        titleColor: 'white',
-        titleSize: '16px',
-        messageColor: 'white',
-        backgroundColor: '#ef4040',
-        iconUrl: iconOctagon,
-      });
-    })
-    .finally(() => {});
-}
-
-function createPromise(delay, stateArr) {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (stateArr.value === 'fulfilled') {
+      if (state === 'fulfilled') {
         resolve(delay);
       } else {
         reject(delay);
       }
     }, delay);
   });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
 }
-
-// function handlerCreatePromise(evt) {
-//   evt.preventDefault();
-//   const { delay, state } = evt.target.elements;
-//   const delayNumber = Number(delay.value);
-//   const stateArr = [...state].find(item => item.checked);
-
-//   setTimeout(() => {
-//     const promise = new Promise((resolve, reject) => {
-//       if (stateArr.value === 'fulfilled') {
-//         resolve(console.log(delayNumber));
-//         iziToast.success({
-//           position: 'topRight',
-//           title: 'OK',
-//           message: `Fulfilled promise in ${delayNumber}ms`,
-//           titleColor: 'white',
-//           titleSize: '16px',
-//           messageColor: 'white',
-//           backgroundColor: '#59a10d',
-//           iconUrl: '../img/bi_check2-circle.png',
-//         });
-//       } else {
-//         reject(delayNumber);
-//         iziToast.error({
-//           position: 'topRight',
-//           title: 'Error',
-//           message: `Rejected promise in ${delayNumber}ms`,
-//           titleColor: 'white',
-//           titleSize: '16px',
-//           messageColor: 'white',
-//           backgroundColor: '#ef4040',
-//           iconUrl: '../img/bi_x-octagon.png',
-//         });
-//       }
-//     });
-//   }, delayNumber);
-// }
